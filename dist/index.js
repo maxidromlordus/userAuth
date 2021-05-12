5204166c7a68263a10e9a92d996c68635d3a32f7
Maxim Samohvalov <maxidromlordus@gmail.com>
2021-05-12 16:15:26 +0300

    forms  done

--- dist/index.js
+++ dist/index.js
@@ -1,19 +1,67 @@
 "use strict";
 
-var form = document.querySelector(".signIn-form");
-form.addEventListener("submit", validateForm, false);
+var form = document.querySelector(".signIn-form"); //form.addEventListener("submit", validateForm, false);
+
 var loginInput = document.getElementById("login");
 var loginPassword = document.getElementById("password");
 var labelPassword = document.querySelector(".label-password");
-var labelInput = document.querySelector(".label-input"); //toolTip for formField listeners
+var labelInput = document.querySelector(".label-input");
+var wrapper = document.getElementById("wrapper");
+var loginWrapper = document.getElementById("login-wrapper");
+var signinWrapper = document.getElementById("signin-wrapper");
+var pLoginText = document.getElementById("p-login");
+var pSigninText = document.getElementById("p-signin");
+var formWrapper = document.createElement("div");
+var loginFormDiv = document.createElement("div");
+var signinFormDiv = document.createElement("div");
+loginFormDiv.id = "login-in";
+signinFormDiv.id = "sign-in";
+var loginForm = "<form onsubmit=\"event.preventDefault();\" class=\"signIn-form\" name=\"form\">\n  <label for=\"login\" class=\"label label-input\">Login:</label>\n  <input type=\"text\" name=\"login\"  id=\"login\"  pattern=\"[a-zA-Z0-9]+\" minlength=\"4\" maxlength=\"12\" /><br />\n  <label for=\"password\" class=\"label label-password\">Password:</label>\n  <input type=\"password\" name=\"password\" id=\"password\"/><br />\n  <input type=\"submit\" value=\"Login\" class=\"submit-input\"></input>\n</form>";
+var signinForm = "<form onsubmit=\"event.preventDefault();\" class=\"signIn-form\" name=\"form\">\n  <label for=\"login\" class=\"label label-input\">Login:</label>\n  <input type=\"text\" name=\"login\"  id=\"login\"  pattern=\"[a-zA-Z0-9]+\" minlength=\"4\" maxlength=\"12\" /><br />\n  <label for=\"password\" class=\"label label-password\">Password:</label>\n  <input type=\"password\" name=\"password\" id=\"password\"/><br />\n  <input type=\"submit\" value=\"Signin\" class=\"submit-input\"></input>\n</form>";
+loginFormDiv.innerHTML = loginForm;
+signinFormDiv.innerHTML = signinForm; // console.log();
+// Forms Event listeners.
 
-loginInput.addEventListener("mouseover", function () {
-  form.login.value = "4-12 characters";
-  console.log("over");
+loginWrapper.addEventListener("mouseenter", function () {
+  if (loginWrapper.contains(pLoginText)) {
+    loginWrapper.removeChild(pLoginText);
+    loginWrapper.appendChild(loginFormDiv);
+  }
 });
-loginInput.addEventListener("mouseout", function () {
-  form.login.value = "";
+loginWrapper.addEventListener("mouseleave", function () {
+  var focused = document.activeElement === login;
+  document.activeElement.blur();
+  console.log(focused);
+
+  if (loginWrapper.contains(loginFormDiv) && !focused) {
+    console.log("contains");
+    loginWrapper.removeChild(loginFormDiv);
+    loginWrapper.appendChild(pLoginText);
+  }
 });
+signinWrapper.addEventListener("mouseenter", function () {
+  if (signinWrapper.contains(pSigninText)) {
+    signinWrapper.removeChild(pSigninText);
+    signinWrapper.appendChild(signinFormDiv);
+  }
+});
+signinWrapper.addEventListener("mouseleave", function () {
+  var focused = document.activeElement === login;
+  console.log(focused);
+
+  if (signinWrapper.contains(signinFormDiv) && !focused) {
+    console.log("contains");
+    signinWrapper.removeChild(signinFormDiv);
+    signinWrapper.appendChild(pSigninText);
+  }
+}); //toolTip for formField listeners
+// loginInput.addEventListener("mouseover", () => {
+//   form.login.value = "4-12 characters";
+//   console.log("over");
+// });
+// loginInput.addEventListener("mouseout", () => {
+//   form.login.value = "";
+// });
 
 function validateForm() {
   var loginValue = form.elements.login.value;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,87 @@
 const form = document.querySelector(".signIn-form");
-form.addEventListener("submit", validateForm, false);
+//form.addEventListener("submit", validateForm, false);
 const loginInput = document.getElementById("login");
 const loginPassword = document.getElementById("password");
 const labelPassword = document.querySelector(".label-password");
 const labelInput = document.querySelector(".label-input");
+const wrapper = document.getElementById("wrapper");
+const loginWrapper = document.getElementById("login-wrapper");
+const signinWrapper = document.getElementById("signin-wrapper");
+const pLoginText = document.getElementById("p-login");
+const pSigninText = document.getElementById("p-signin");
+
+const formWrapper = document.createElement("div");
+const loginFormDiv = document.createElement("div");
+const signinFormDiv = document.createElement("div");
+loginFormDiv.id = "login-in";
+signinFormDiv.id = "sign-in";
+
+const loginForm = `<form onsubmit="event.preventDefault();" class="signIn-form" name="form">
+  <label for="login" class="label label-input">Login:</label>
+  <input type="text" name="login"  id="login"  pattern="[a-zA-Z0-9]+" minlength="4" maxlength="12" /><br />
+  <label for="password" class="label label-password">Password:</label>
+  <input type="password" name="password" id="password"/><br />
+  <input type="submit" value="Login" class="submit-input"></input>
+</form>`;
+
+const signinForm = `<form onsubmit="event.preventDefault();" class="signIn-form" name="form">
+  <label for="login" class="label label-input">Login:</label>
+  <input type="text" name="login"  id="login"  pattern="[a-zA-Z0-9]+" minlength="4" maxlength="12" /><br />
+  <label for="password" class="label label-password">Password:</label>
+  <input type="password" name="password" id="password"/><br />
+  <input type="submit" value="Signin" class="submit-input"></input>
+</form>`;
+
+loginFormDiv.innerHTML = loginForm;
+signinFormDiv.innerHTML = signinForm;
+// console.log();
+
+// Forms Event listeners.
+loginWrapper.addEventListener("mouseenter", () => {
+  if (loginWrapper.contains(pLoginText)) {
+    loginWrapper.removeChild(pLoginText);
+    loginWrapper.appendChild(loginFormDiv);
+  }
+});
+
+loginWrapper.addEventListener("mouseleave", () => {
+  let focused = document.activeElement === login;
+  document.activeElement.blur();
+  console.log(focused);
+  if (loginWrapper.contains(loginFormDiv) && !focused) {
+    console.log("contains");
+    loginWrapper.removeChild(loginFormDiv);
+    loginWrapper.appendChild(pLoginText);
+  }
+});
+
+signinWrapper.addEventListener("mouseenter", () => {
+  if (signinWrapper.contains(pSigninText)) {
+    signinWrapper.removeChild(pSigninText);
+    signinWrapper.appendChild(signinFormDiv);
+  }
+});
+
+signinWrapper.addEventListener("mouseleave", () => {
+  let focused = document.activeElement === login;
+  console.log(focused);
+  if (signinWrapper.contains(signinFormDiv) && !focused) {
+    console.log("contains");
+    signinWrapper.removeChild(signinFormDiv);
+    signinWrapper.appendChild(pSigninText);
+  }
+});
 
 //toolTip for formField listeners
 
-loginInput.addEventListener("mouseover", () => {
-  form.login.value = "4-12 characters";
-  console.log("over");
-});
+// loginInput.addEventListener("mouseover", () => {
+//   form.login.value = "4-12 characters";
+//   console.log("over");
+// });
 
-loginInput.addEventListener("mouseout", () => {
-  form.login.value = "";
-});
+// loginInput.addEventListener("mouseout", () => {
+//   form.login.value = "";
+// });
 
 function validateForm() {
   var loginValue = form.elements.login.value;
